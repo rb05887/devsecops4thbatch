e7b8ada16aee63c91a4a45c244cc0d63c45d46f9
Raj <rb058877@gmail.com>
2023-05-19 18:36:33 +0530

    this is another change with B.java

--- A.java
+++ A.java
@@ -2,4 +2,10 @@ class A {
      method1() {
              System.out.println("in method1"); 
                 }
+                method2(){
+                        system.out.println("in method2");
+                }
+                method3(){
+                        system.out.println("in method3");
+                }
         }
---
+++ B.java
@@ -0,0 +1,6 @@
+class B {
+     method10() {
+             System.out.println("in method10"); 
+                
+                }
+        }
