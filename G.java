b4bf42ff66decffd4b1a8c3dd10451701f7f2c20
Raj <rb058877@gmail.com>
2023-05-20 17:38:11 +0530

    new G file

---
+++ G.java
@@ -0,0 +1,10 @@
+class G {
+     method22() {
+             System.out.println("in method22"); 
+                
+                }
+                method1000() {
+             System.out.println("in method1000"); 
+                
+                }
+        }
