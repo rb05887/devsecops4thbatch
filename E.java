60bd4ca37ff9fc3e8b34a332db574e7fba5028ee
Raj <rb058877@gmail.com>
2023-05-20 17:23:51 +0530

    add new file E.Java

---
+++ E.java
@@ -0,0 +1,10 @@
+class E {
+     method22() {
+             System.out.println("in method22"); 
+                
+                }
+                method220() {
+             System.out.println("in method220"); 
+                
+                }
+        }
