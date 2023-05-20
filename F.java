d954dc3b31d99aa700a761f5f86b9d7d8dc00760
Raj <rb058877@gmail.com>
2023-05-20 17:34:31 +0530

    change in F

---
+++ F.java
@@ -0,0 +1,10 @@
+class F {
+     method22() {
+             System.out.println("in method22"); 
+                
+                }
+                method100() {
+             System.out.println("in method100"); 
+                
+                }
+        }
